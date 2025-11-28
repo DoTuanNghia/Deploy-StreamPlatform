04e5359c7c7900f164f844ee4f14cb9081173cf2
TuanNghia <dtn752k4@gmail.com>
2025-11-28 16:18:33 +0700

    update createStreamSession

--- Backend/src/main/java/com/stream/backend/repository/StreamSessionRepository.java
+++ Backend/src/main/java/com/stream/backend/repository/StreamSessionRepository.java
@@ -2,6 +2,7 @@ package com.stream.backend.repository;
 
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.stream.backend.entity.StreamSession;
@@ -9,4 +10,6 @@ import com.stream.backend.entity.StreamSession;
 public interface StreamSessionRepository extends JpaRepository<StreamSession, Integer>{
     List<StreamSession> findAll();
     List<StreamSession> findByDeviceId(Integer deviceId);
+    List<StreamSession> findByStreamId(Integer streamId);
+    StreamSession save(StreamSession streamSession);
 }
--- Backend/src/main/java/com/stream/backend/service/StreamSessionService.java
+++ Backend/src/main/java/com/stream/backend/service/StreamSessionService.java
@@ -7,4 +7,6 @@ import com.stream.backend.entity.StreamSession;
 public interface StreamSessionService {
     List<StreamSession> getAllStreamSessions();
     List<StreamSession> getStreamSessionsByDeviceId(Integer deviceId);
+    List<StreamSession> getStreamSessionsByStreamId(Integer streamId);
+    StreamSession creatStreamSession(StreamSession streamSession, Integer deviceId, Integer streamId);
 }
