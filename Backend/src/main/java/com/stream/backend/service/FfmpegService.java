c7fa145522c267f8436bbf0709cd1f15b29eaba2
TuanNghia <dtn752k4@gmail.com>
2025-12-18 17:04:43 +0700

    update thông số hiển thị trên streamSession

--- Backend/src/main/java/com/stream/backend/service/FfmpegService.java
+++ Backend/src/main/java/com/stream/backend/service/FfmpegService.java
@@ -12,5 +12,7 @@ public interface FfmpegService {
     void startStream(String videoPath, String rtmpUrl, String streamKey);
 
     void stopStream(String streamKey);
+
+    FfmpegStat getLatestStat(String streamKey);
 }
 
